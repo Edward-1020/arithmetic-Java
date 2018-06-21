074fc781ea8e2e6089082b9e797bd4177bf09596
young <yangxu@yeezon.com>
2018-06-21 08:33:01 +0800

    feat: 数组

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -23,7 +23,59 @@ public class Array {
         return data.length;
     }
 
+    // 是否为空
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    // 添加
+    public void addLast(int e) {
+        add(size, e);
+    }
+
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+    // 插入
+    public void add(int index, int e) {
+        if (size == data.length)
+            throw new IllegalArgumentException("AddLast failed. Array is full");
+
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("AddLast failed. Array is full");
+
+
+        for (int i = size - 1; i >= index; i--)
+            data[i + 1] = data[i];
+
+        data[index] = e;
+        size ++;
+    }
+
+    int get(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Index is illegal");
+        return data[index];
+    }
+
+    void Set(int index, int e) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Index is illegal");
+        data[index] = e;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d, capacity = %d\n", size, data.length));
+        res.append('[');
+        for (int i = 0; i < size; i ++) {
+            res.append(data[i]);
+            if (i != size - 1)
+                res.append(", ");
+        }
+        res.append("]");
+        return res.toString();
     }
 }
--- Array/src/Main.java
+++ Array/src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+    Array arr = new Array(20);
+    for(int i = 0; i < 10; i++)
+        arr.addLast(i);
+    System.out.println(arr);
     }
 }
