b00d1f375a657cb4bb85cf1d4303b243b7d6510e
young <yangxu@yeezon.com>
2018-07-08 22:20:26 +0800

    feat: 循环数组

--- Array/src/Solution.java
+++ Array/src/Solution.java
@@ -2,6 +2,7 @@ public class Solution {
     public boolean isValid(String s) {
         ArrayStack<Character> stack = new ArrayStack<>();
         for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
             } else {
