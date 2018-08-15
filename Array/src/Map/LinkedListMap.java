6f8b9c0082d5e27134a132fcccc2f3fa04292936
young <yangxu@yeezon.com>
2018-08-15 08:37:40 +0800

    feat: clear code

--- Array/src/Map/LinkedListMap.java
+++ Array/src/Map/LinkedListMap.java
@@ -1,6 +1,6 @@
 package Map;
 
-public class LinkedListMap<K, V> implements Map<K, V>{
+public class LinkedListMap<K, V> implements Map<K, V> {
     private class Node {
         public K key;
         public V value;
@@ -26,76 +26,80 @@ public class LinkedListMap<K, V> implements Map<K, V>{
 
     }
 
-        private Node dummyHead;
-        private int size;
+    private Node dummyHead;
+    private int size;
 
-        public void LinkedListMap () {
-            dummyHead = new Node();
-            size = 0;
+    public void LinkedListMap() {
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private Node getNode(K key) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.key.equals(key))
+                return cur;
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getNode(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        Node node = getNode(key);
+        return node == null ? null : node.value;
+    }
+
+    @Override
+    public void add(K key, V value) {
+        Node node = getNode(key);
+        if (node == null) {
+            dummyHead.next = new Node(key, value, dummyHead.next);
+            size++;
+        } else {
+            node.value = value;
+        }
+    }
+
+    @Override
+    public void set(K key, V newValue) {
+        Node node = getNode(key);
+        if (node == null)
+            throw new IllegalArgumentException(key + " doesn't exist!");
+        node.value = newValue;
+    }
+
+    @Override
+    public V remove(K key) {
+        Node prev = dummyHead;
+
+        while (prev.next != null) {
+            if (prev.next.key.equals(key))
+                break;
+            prev = prev.next;
         }
 
-        @Override
-        public int getSize() {return size;}
-
-        @Override
-        public boolean isEmpty() {return size == 0;}
-
-        private Node getNode (K key) {
-            Node cur = dummyHead.next;
-            while(cur != null) {
-                if (cur.key.equals(key))
-                    return cur;
-                cur = cur.next;
-            }
-            return null;
+        if (prev.next != null) {
+            Node delNode = prev.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+            return delNode.value;
         }
-
-        @Override
-        public boolean contains (K key) {
-            return getNode(key) != null;
-        }
-
-        @Override
-        public V get (K key) {
-            Node node = getNode(key);
-            return node == null ?  null : node.value;
-        }
-
-        @Override
-        public void add (K key, V value) {
-            Node node = getNode(key);
-            if (node == null) {
-                dummyHead.next = new Node(key, value, dummyHead.next);
-                size++;
-            } else {
-                node.value = value;
-            }
-        }
-
-        @Override
-        public void set (K key, V newValue) {
-            Node node = getNode(key);
-            if (node == null)
-                throw new IllegalArgumentException(key + " doesn't exist!");
-            node.value = newValue;
-        }
-
-        @Override
-        public V remove (K key) {
-            Node prev = dummyHead;
-
-            while (prev.next != null) {
-                if (prev.next.key.equals(key))
-                    break;
-                prev = prev.next;
-            }
-
-            if (prev.next != null) {
-                Node delNode = prev.next;
-                prev.next = delNode.next;
-                delNode.next = null;
-                return delNode.value;
-            }
-            return null;
-        }
+        return null;
+    }
 }
