b565faf2bbe1e17f699ed34f7a9e5d0d12dd0499
YangXu <yangxu@baijiahulian.com>
2018-08-20 13:27:54 +0800

    feat: 线段树区间查询

--- Array/src/SegmentTree/SegmentTree.java
+++ Array/src/SegmentTree/SegmentTree.java
@@ -54,6 +54,36 @@ public class SegmentTree<E>{
         return 2 * index + 2;
     }
 
+    //  返回区间[queryL, queryR]的值
+    public E query (int queryL, int queryR) {
+        if (queryL < 0 || queryL >= data.length ||
+            queryR < 0 || queryR >= data.length || queryL > queryR) {
+                throw new Error(`Index is illegal`);
+            }
+        
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+    
+    //  在以treeID为根的线段树中[l...r]的范围里，搜索区间[queryL...queryR]的值
+    private E query (int treeIndex, int l, int r, int queryL, int queryR) {
+        if (l == queryL && r == queryR) {
+            return tree[treeIndex];
+        }
+
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        if (queryL >= mid + 1)
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+        else if (queryR <= mid)
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+
+        E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+        return merger.merge(leftResult, rightResult);
+    }
+
     @Override
     public String toString () {
         StringBuilder res = new StringBuilder();
@@ -80,6 +110,6 @@ public class SegmentTree<E>{
             }
         });
 
-        System.out.println(segTree);
+        System.out.println(segTree.query(0, 2));
     }
 }
