78a92c59e62b4a2dc4ae5154664f8cf7a832e6b2
young <iezzuyoung@gmail.com>
2018-08-21 13:41:34 +0800

    线段树的题,数据可改变

--- Array/src/SegmentTree/leetcodeSolutionNoSegmentTree.java
+++ Array/src/SegmentTree/leetcodeSolutionNoSegmentTree.java
@@ -12,16 +12,26 @@
 //  时间复杂度更低
 class NumArray {
     private int[] sum;//  sum[i]存储前i个元素和
+    private int[] data;
 
     private SegmentTree<Integer> segmentTree;
 
     public NumArray (int[] nums) {
+        data = new int[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            data[i] = nums[i];
 
         sum = new int[num.length + 1];
         sum[0] = 0;
 
         for (int i = 1; i < sum.length; i++)
             sum[i] = sum[i - 1] + nums[i - 1];
+    }
+
+    public void update (int index, int val) {
+        data[index] = val;
+        for (int i = index + 1; i < sum.length; i++)
+            sum[i] = sum[i - 1] + data[i - 1];
     }
 
     public int sumRange (int i, int j) {
