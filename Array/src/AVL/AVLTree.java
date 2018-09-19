708bf09a0f323fbbe7cf1aecce43bcb2abff7959
young <yangxu@yeezon.com>
2018-09-19 08:37:02 +0800

    feat: 左旋转和右旋转

--- Array/src/AVL/AVLTree.java
+++ Array/src/AVL/AVLTree.java
@@ -84,6 +84,39 @@ public class AVLTree<K extends Comparable<K>, V>{
         return getHeight(node.left) - getHeight(node.right);
     }
 
+    //  对节点y进行向右旋转操作，返回旋转后新的根节点x
+    private Node rightRotate (Node y) {
+        Node x = y.left;
+        Node T3 = x.right;
+
+        //  向右旋转过程
+        x.right = y;
+        y.left = T3;
+
+        //  更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    //  对节点y进行左旋转操作，返回旋转后新的根节点x
+    private Node leftRotate (Node y) {
+        Node x = y.right;
+        Node T2 = x.left;
+
+        //  向左旋转过程
+        x.left = y;
+        y.right = T2;
+
+        //  更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+
     public void add (K key, V value) {
         root = add(root, key ,value);
     }
@@ -110,7 +143,7 @@ public class AVLTree<K extends Comparable<K>, V>{
         //  更新height
         node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
 
-        //  平衡因子
+        //  计算平衡因子
         int balanceFactor = getBalanceFactor(node);
         if (Math.abs(balanceFactor) > 1) {
             System.out.println("unbalanced:" + balanceFactor);
@@ -120,6 +153,11 @@ public class AVLTree<K extends Comparable<K>, V>{
         //  当前节点平衡因子大于1，不平衡。此时，左子树的平衡因子 》= 0, 及左子树高度大于右子树高度
         //  右旋转
         if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+            return rightRotate(node);
+
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0) {
+            return leftRotate(node);
+        }
 
         return node;
     }
